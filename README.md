69badabedf0f0701b4d18ac09fa48ea4e902f3cf
Arpad Cseh <arpad.cseh@gmail.com>
2018-03-27 14:16:40 +0200

    add coverage task

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # js-exercises
+
+[![Build Status](https://travis-ci.org/arpadcseh/js-exercises.svg?branch=master)](https://travis-ci.org/arpadcseh/js-exercises)
+
 Just playing around with JS
--- freeCodeCamp/no-repeats-please-test.ts
+++ freeCodeCamp/no-repeats-please-test.ts
@@ -2,14 +2,14 @@ import test from 'ava';
 import permAlone from './no-repeats-please';
 
 test('no repeats please', t => {
-    t.is(permAlone("aab"), 2, 'should return a number.');
-    t.is(permAlone("aab"), 2, 'should return 2.');
-    t.is(permAlone("aaa"), 0, 'should return 0.');
-    t.is(permAlone("aabb"), 8, 'should return 8.');
-    t.is(permAlone("abcdefa"), 3600, 'should return 3600.');
-    t.is(permAlone("abfdefa"), 2640, 'should return 2640.');
-    t.is(permAlone("zzzzzzzz"), 0, 'should return 0.');
-    t.is(permAlone("a"), 1, 'should return 1.');
-    t.is(permAlone("aaab"), 0, 'should return 0.');
-    t.is(permAlone("aaabb"), 12, 'should return 12.');
+    t.is(permAlone('aab'), 2, 'should return a number.');
+    t.is(permAlone('aab'), 2, 'should return 2.');
+    t.is(permAlone('aaa'), 0, 'should return 0.');
+    t.is(permAlone('aabb'), 8, 'should return 8.');
+    t.is(permAlone('abcdefa'), 3600, 'should return 3600.');
+    t.is(permAlone('abfdefa'), 2640, 'should return 2640.');
+    t.is(permAlone('zzzzzzzz'), 0, 'should return 0.');
+    t.is(permAlone('a'), 1, 'should return 1.');
+    t.is(permAlone('aaab'), 0, 'should return 0.');
+    t.is(permAlone('aaabb'), 12, 'should return 12.');
 });
